3e30d77491de773688e7a390f12345e87e681bbc
Amrit <amritkarn363@gmail.com>
2026-08-19 15:59:49 +0530

    day 11 end

---
+++ day 11/work/LC 112/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        // We reached a leaf
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        targetSum -= root.val;
+
+        return hasPathSum(root.left, targetSum)
+            || hasPathSum(root.right, targetSum);
+    }
+}
---
+++ day 11/work/LC 543/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        // Diameter passing through this node
+        diameter = Math.max(
+            diameter,
+            leftHeight + rightHeight
+        );
+
+        // Return height to parent
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
